4b1107cbee7ff51613ab6b7184a5a47ddfd732ca
Kei Duke Bergman <54135223+Keiotic@users.noreply.github.com>
2023-03-08 19:05:48 +0100

    Reinstate my laser code

--- Cloud/Assets/Scripts/AbsorptionMap.cs
+++ Cloud/Assets/Scripts/AbsorptionMap.cs
@@ -6,8 +6,8 @@ public class AbsorptionMap : MonoBehaviour
 {
     [SerializeField] private Texture2D xy;
 
-    [SerializeField] private float densityModifier;
     [SerializeField] private Vector3 bounds = Vector3.one;
+    public Vector3 Bounds { get { return bounds; } }
     [SerializeField] private float concentrationFactor = 1.0f;
     [SerializeField] private float attenuationFactor = 1.0f;
     // Start is called before the first frame update
@@ -22,20 +22,23 @@ public class AbsorptionMap : MonoBehaviour
         
     }
 
-    public float GetAbsorption (Vector3 position)
+    public float GetAbsorption (Vector2 position)
     {
+
         float grayscale = 1;
         if(xy == null) {
-            return grayscale * densityModifier;
+            return 0;
         }
-        
-        Vector3 samplePosition = transform.InverseTransformPoint(position);
+
+        Vector2 samplePosition = transform.InverseTransformPoint(position);
+        samplePosition.x += 0.5f;
+        samplePosition.y += 0.5f;
+
         Vector2 uvPosition = new Vector2(samplePosition.x / bounds.x * xy.width,
                                             samplePosition.y / bounds.y * xy.height);
         grayscale = xy.GetPixel(Mathf.RoundToInt(uvPosition.x), 
-            Mathf.RoundToInt(uvPosition.y)).grayscale * densityModifier;
-
+            Mathf.RoundToInt(uvPosition.y)).grayscale;
         float concentration = 1 - grayscale;
-        return concentration;
+        return concentration*concentrationFactor*attenuationFactor;
     }
 }
--- Cloud/Assets/Scripts/lightBehavior.cs
+++ Cloud/Assets/Scripts/lightBehavior.cs
@@ -2,20 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
-
 public class lightBehavior : MonoBehaviour
 {
-    [SerializeField] private int m_segmentCount = 20; //idk
+    [SerializeField] private int m_segmentCount = 20;
     [SerializeField] private float m_rayLength = 4f;
+    [SerializeField] private GameObject m_cloud;
+    [SerializeField] private GameObject m_line;
+    private GameObject[] m_lines;
 
-    [SerializeField] private GameObject cloud;
+    private float m_lineSize = 0;
 
-
-    [SerializeField] private GameObject line;
-    private GameObject[] lines;
-
-    private AbsorptionMap am;
+    private AbsorptionMap m_absorptionMap;
+    [SerializeField] private Color m_rayColor = Color.white;
 
 
 
@@ -23,44 +21,85 @@ public class lightBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        am = cloud.GetComponent<AbsorptionMap>();
-         
+        m_absorptionMap = m_cloud.GetComponent<AbsorptionMap>();
+        m_lineSize = m_line.transform.localScale.z * 10;
+        int totalLines = Mathf.RoundToInt(m_rayLength / m_lineSize);
+        m_lines = new GameObject[totalLines];
+
+        for (int i = 0; i < totalLines; i++)
+        {
+            m_lines[i] = Instantiate(m_line, transform.position, Quaternion.Euler(180, 0, -90));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateLines();
         RenderLine();
+    }
+
+    void UpdateLines()
+    {
+        Vector3 position = transform.position;
+        for(int i = 0; i < m_lines.Length; i++)
+        {
+            position += m_lineSize * transform.forward;
+            m_lines[i].transform.position = position;
+        }
+    }
+
+    void ChangeLineColor(int from, int to, Color color)
+    {
+        for(int i = from; i < to; i++){
+            m_lines[i].GetComponent<Renderer>().material.color = color; 
+        }
     }
 
     void RenderLine()
     {
         Vector3 samplePosition = transform.position;
-        float stepLength = m_rayLength/m_segmentCount;
+        
         RaycastHit hit;
-
         if(Physics.Raycast(transform.position, transform.forward, out hit))
         {
             samplePosition = hit.point;
         }
         else
         {
+            ChangeLineColor(0, m_lines.Length, m_rayColor);
             return;
         }
 
+        int lastUpdatedLine = 0;
+        int currentLine = Mathf.RoundToInt(hit.distance/m_lineSize);
+
+        Color rayColor = m_rayColor;
+        ChangeLineColor(lastUpdatedLine, currentLine, rayColor);
 
 
+        float stepLength = m_absorptionMap.Bounds.x / m_segmentCount;
         float riemannsum = 0;
+
+        int linesPerStepLength = Mathf.RoundToInt(stepLength / m_lineSize);
+        lastUpdatedLine = currentLine;
+
         for (int i = 0; i < m_segmentCount; i++){
             samplePosition += (transform.forward * stepLength);
 
             if (i == 0)
                 continue;
+            float absorption = m_absorptionMap.GetAbsorption(new Vector2(samplePosition.z, samplePosition.y));
 
-            float absorption = am.GetAbsorption(samplePosition);
-       
-
+            riemannsum += absorption * stepLength;
+            Debug.Log(riemannsum);
+            currentLine += linesPerStepLength;
+            rayColor.a = Mathf.Pow(10, -riemannsum);
+            ChangeLineColor(lastUpdatedLine, currentLine, rayColor);
+            lastUpdatedLine = currentLine;
         }
-        
+
+        rayColor.a = Mathf.Pow(10, -riemannsum);
+        ChangeLineColor(lastUpdatedLine, m_lines.Length, rayColor);
     }
 }
